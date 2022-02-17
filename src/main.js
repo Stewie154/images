770eeb58ddd0bf3b1698cf4c01b5ef3f4514ff55
Stewart McFarlane <stewart@toru.digital>
2022-02-17 12:28:52 +0000

    setup to link vue with vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import Vue from "vue";
-import App from "./App";
+import Vue from "vue"
+import App from "./App"
+//webpack automatically imports the /index.js file, which is why the import stops at ./store
+import store from "./store"
 
 new Vue({
+    store, 
     render: h => h(App)
 }).$mount('#app')
