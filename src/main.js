ef65d0717b3dfef42252ea66608e68bd55b96ab9
Stewart McFarlane <stewart@toru.digital>
2022-02-17 16:24:01 +0000

    added VueRouter & created AuthHandler component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from "vue"
 import App from "./App"
+import VueRouter from "vue-router"
 //webpack automatically imports the /index.js file, which is why the import stops at ./store
 import store from "./store"
+import AuthHandler from './components/AuthHandler'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/oauth2/callback',
+            component: AuthHandler
+        }
+    ]
+})
 
 new Vue({
+    router,
     store, 
     render: h => h(App)
 }).$mount('#app')
