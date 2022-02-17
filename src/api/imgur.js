c9fb73239ac8da981c7ba407158c34201fb1ec0e
Stewart McFarlane <stewart@toru.digital>
2022-02-17 14:50:15 +0000

    removed redundant url

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -3,7 +3,6 @@ import qs from 'qs'
 const CLIENT_ID = '1033af3e4cb9162'
 const ROOT_URL = 'https://api.imgur.com'
 
-https://api.imgur.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&response_type=REQUESTED_RESPONSE_TYPE&state=APPLICATION_STATE
 export default {
     login() {
         const querystring = {
