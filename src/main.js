aaf77398dd7365d9839f11376a3e61504ee39722
Stewart McFarlane <stewart@toru.digital>
2022-02-23 15:11:47 +0000

    updated routes to include new components

--- src/main.js
+++ src/main.js
@@ -4,12 +4,22 @@ import VueRouter from "vue-router"
 //webpack automatically imports the /index.js file, which is why the import stops at ./store
 import store from "./store"
 import AuthHandler from './components/AuthHandler'
+import ImageList from './components/ImageList'
+import UploadForm from './components/UploadForm'
 
 Vue.use(VueRouter)
 
 export const router = new VueRouter({
     mode: 'history',
     routes: [
+        {
+            path: '/',
+            component: ImageList
+        },
+        {
+            path: '/upload',
+            component: UploadForm
+        },
         {
             path: '/oauth2/callback',
             component: AuthHandler
