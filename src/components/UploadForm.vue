2087b29a5f2ef02a589961df526251a247b611a6
Stewart McFarlane <stewart@toru.digital>
2022-07-13 16:48:16 +0100

    added html for drag and drop uploader

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>Upload Form</div>
+	<div class="dropper">
+		<input type="file" />
+		<span>Drag files here!</span>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: 'UploadForm'
-    }
+export default {
+	name: 'UploadForm'
+}
 </script>
+<style scoped>
+.dropper {
+	height: 30vh;
+	border: 2px dashed black;
+	border-radius: 5px;
+}
+</style>
